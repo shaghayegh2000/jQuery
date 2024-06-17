17c273f099836d2d4576cf83eefc65009da5824a
shaghaygh <shqayqm145@gmail.com>
2024-06-17 20:00:32 +0330

    17/6/2024

--- learning/script.js
+++ learning/script.js
@@ -1,4 +1,71 @@
+// $(document).ready(function() {
+//     $('p').fadeOut(2000);
+// });
 
-$(document).ready(function() {
-    $('p').fadeOut(2000);
-});
+// $('.button').click(function (e) { 
+//     e.preventDefault();
+//     $('.well').fadeOut(3000);
+// });
+
+// $('.well').hide();
+// $(window).ready(function() {
+//     $('.well').fadeIn(3000);
+// });
+
+// $('.button2').click(function (e) { 
+//     e.preventDefault();
+//     $('*').fadeOut(3000);
+// });
+
+// $('.button2').click(function (e) { 
+//     e.preventDefault();
+//     $("[test*='test']").fadeOut(3000);
+// });
+
+// $('.button3').click(function (e) { 
+//     e.preventDefault();
+//     $("[wordname ~= 'word']").fadeOut(3000);
+// });
+
+// $('.button4').click(function (e) { 
+//     e.preventDefault();
+//     $("[endWith $= 'name']").fadeOut(3000);
+// });
+
+// $('.button5').click(function (e) { 
+//     e.preventDefault();
+//     $("[startWith ^= 'word']").fadeOut(3000);
+// });
+
+// $('.button6').click(function (e) { 
+//     e.preventDefault();
+//     $(':button').fadeOut(3000);
+// });
+
+// const button = document.querySelectorAll('button');
+// const a = document.querySelector('.button6');
+// a.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     button.forEach(btn => {
+//         console.log(btn.classList[1].charAt(3));
+//         setTimeout(() => {
+//             $(btn).fadeOut(500*(btn.classList[1].charAt(3))); 
+//         }, 1000*(btn.classList[1].charAt(3)));
+//     });
+// })
+
+// $('.button6').click(function (e) { 
+//     e.preventDefault();
+//     $(".parent > *").fadeOut(2000);
+// });
+
+// $('.button6').click(function (e) { 
+//     e.preventDefault();
+//     $(".parent:contains('button')").fadeOut(3000);
+// });
+
+// $('.text').click(function (e) { 
+//     e.preventDefault();
+//     $("input:blur").fadeOut(3000);
+
+// });
